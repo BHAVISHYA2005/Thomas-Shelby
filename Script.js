3d4baa36140a2cd23b672c61673e340bd8dba772
Bhavishya <handballaggarwal@gmail.com>
2024-09-03 13:42:10 +0530

    added isAbove18 fn

---
+++ Script.js
@@ -0,0 +1,15 @@
+
+const name1 = "Bhavishya"
+const age = 19
+const dob = '07/03/2005'
+
+function sayName(){
+    console.log('My name is ' + name1)
+}
+
+
+
+function isAbove18() {
+    return age >=  18
+
+}
